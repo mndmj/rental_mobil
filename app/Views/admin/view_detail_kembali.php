3dc34f0183df2bb5ed6ebc87de48916ee3801188
mndmj <nasrullah.03.arul@gmail.com>
2023-07-26 21:18:45 +0700

    tampilan detail kerusakan

--- app/Views/admin/view_detail_kembali.php
+++ app/Views/admin/view_detail_kembali.php
@@ -5,10 +5,6 @@
     <div class="card card-dark mb-0">
         <div class="card-header">
             <h3 class="card-title">Daftar <?= $subtitle ?></h3>
-            <div class="card-tools">
-                <button class="btn btn-sm btn-warning" data-toggle="modal" data-target="#edit">
-                    <i class="fa fa-pen" aria-hidden="true"></i> Edit</button>
-            </div>
         </div>
     </div>
     <div class="card-body pt-0 rounded-bottom shadow-sm border-dark border">
@@ -87,115 +83,84 @@
                 </div>
             </div>
         </div>
-    </div>
-</div>
-
-<div>
-    <?php
-    if (!is_null($dtKembali)) {
-        $date = $dtKembali['tgl_kembali'];
-    } else {
-        $date = date("Y-m-d H:i:s");
-    }
-    $sisa = (int)((strtotime($dtTransaksi['tgl_kembali']) - strtotime($date)) / 3600);
-    ?>
-    Pinjam : <?= $dtTransaksi['tgl_pinjam'] ?><br>
-    Kembali : <?= $dtTransaksi['tgl_kembali'] ?><br>
-    Dikembalikan : <?= (is_null($dtKembali)) ? 'Belum' : $date ?><br>
-    Sisa Durasi : <?= ($sisa >= 0) ? $sisa : 0 ?> Jam<br>
-    Keterlambatan : <?= ($sisa < 0) ? '<span class="text-danger">' . $sisa . '</span>' : 0 ?> Jam<br>
-    Denda : Rp <?= ($sisa < 0) ? '<span class="text-danger">' . ($sisa * -1 * 20000) . '</span>' : 0 ?><br>
-    <?php if ($dtTransaksi['status_pinjam'] == "Dipinjam") : ?>
-        <form id="formPengembalian" method="post" action="<?= base_url('transaksi/pengembalian') ?>">
-            <textarea name="kondisi_mobil" id="kondisi_mobil" class="form-control" rows="3" placeholder="Kondisi Mobil"></textarea>
-            <input type="text" name="denda_kerusakan" id="denda_kerusakan" class="form-control" placeholder="Denda kerusakan" required>
-            <textarea name="kerusakan" id="kerusakan" class="form-control" rows="3" placeholder="Kerusakan"></textarea>
-            <div class="form-check-inline">
-                <input type="checkbox" name="crashCar" id="crashCar" value="1"> <label for="crashCar">Mobil rusak</label>
-            </div>
-        </form>
-        <button class="btn btn-primary" onclick="pengembalian()">Kembalikan Mobil</button>
-    <?php endif ?>
-</div>
-
-<!-- Modal Edit Data -->
-<?php //foreach ($pinjam as $key => $value) { 
-?>
-<div class="modal fade" id="edit<?php //$value['id_pinjam'] 
-                                ?>">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header bg-warning">
-                <h4 class="modal-title">Edit Data</h4>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <?php //form_open('transaksi/edit_data/' . $value['id_pinjam']) 
-            ?>
-            <div class="modal-body">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="form-group">
-                            <label for="">Nama</label>
-                            <input name="nama_user" class="form-control" placeholder="Nama" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="">Tanggal Pesan</label>
-                            <input name="tgl_pesan" class="form-control" type="datetime-local" placeholder="Tanggal Pesan" disabled>
-                        </div>
-                        <div class="form-group">
-                            <label for="">Tanggal Pinjam</label>
-                            <input name="tgl_pinjam" class="form-control" type="datetime-local" placeholder="Tanggal Pinjam" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="">Nama Mobil</label>
-                            <select class="form-control" name="nama_mobil" id="nama_mobil" required>
-                                <option value="">--Pilih Mobil--</option>
-                                <?php //foreach ($mobil as $value) : 
-                                ?>
-                                <option value="<?php // $value['id_mobil'] 
-                                                ?>"><?php // $value['nama'] 
-                                                    ?></option>
-                                <?php //endforeach 
-                                ?>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label for="">Telepon</label>
-                            <input name="telepon" class="form-control" placeholder="Telepon" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="">No Polisi</label>
-                            <input name="no_polisi" class="form-control" placeholder="No Polisi" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="">Jaminan</label>
-                            <select class="form-control" name="jaminan" id="jaminan" required>
-                                <option value="">--Pilih Jaminan--</option>
-                                <?php //foreach ($jaminan as $value) : 
-                                ?>
-                                <option value="<?php // $value 
-                                                ?>"><?php //$value 
-                                                    ?></option>
-                                <?php //endforeach 
-                                ?>
-                            </select>
-                        </div>
+        <div class="row mt-4 shadow">
+            <div class="col bg-light rounded p-0">
+                <div class="card bg-danger">
+                    <div class="card-header">
+                        <h3 class="card-title">Detail Kondisi Kendaraan</h3>
                     </div>
                 </div>
+                <div class="card-body pt-0">
+
+                    <?php
+                    if (!is_null($dtKembali)) {
+                        $date = $dtKembali['tgl_kembali'];
+                    } else {
+                        $date = date("Y-m-d H:i:s");
+                    }
+                    $sisa = (int)((strtotime($dtTransaksi['tgl_kembali']) - strtotime($date)) / 3600);
+                    ?>
+
+                    <table class="table table-sm mb-3" id="tbl_tr3">
+                        <thead>
+                            <tr>
+                                <th>Tanggal Pinjam</th>
+                                <th>Tanggal Kembali</th>
+                                <th>Dikembalikan</th>
+                                <th>Sisa Durasi</th>
+                                <th>Keterlambatan</th>
+                                <th>Denda</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><?= $dtTransaksi['tgl_pinjam'] ?></td>
+                                <td><?= $dtTransaksi['tgl_kembali'] ?></td>
+                                <td><?= (is_null($dtKembali)) ? 'Belum' : $date ?></td>
+                                <td><?= ($sisa >= 0) ? $sisa : 0 ?> Jam</td>
+                                <td><?= ($sisa < 0) ? '<span class="text-danger">' . $sisa . '</span>' : 0 ?> Jam</td>
+                                <td>Rp <?= ($sisa < 0) ? '<span class="text-danger">' . ($sisa * -1 * 20000) . '</span>' : 0 ?></td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <?php if ($dtTransaksi['status_pinjam'] == "Dipinjam") : ?>
+                        <form id="formPengembalian" method="post" action="<?= base_url('transaksi/pengembalian') ?>">
+                            <div class="row">
+                                <div class="col-4">
+                                    <div class="form-group">
+                                        <label for="kondisi_mobil">Kondisi mobil</label>
+                                        <textarea class="form-control mb-3" name="kondisi_mobil" id="kondisi_mobil" rows="5" placeholder="Kondisi Mobil"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="form-check">
+                                        <input class="mb-0 form-check-input" type="checkbox" name="crashCar" id="crashCar" value="1">
+                                        <label class="m-0" for="crashCar">Mobil Rusak</label><br>
+                                    </div>
+                                    <small>Centang checklist tersebut ketika mengembalikan mobil dengan keadaan rusak</small>
+                                    <div class="form-group mt-4">
+                                        <label for="denda_kerusakan">Denda kerusakan</label>
+                                        <input class="form-control" type="text" name="denda_kerusakan" id="denda_kerusakan" placeholder="Denda kerusakan" required disabled>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="form-group">
+                                        <label for="kerusakan">Kerusakan pada mobil</label>
+                                        <textarea class="form-control mb-3" name="kerusakan" id="kerusakan" rows="5" placeholder="Kerusakan" disabled></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                        <div class="w-100 d-flex">
+                            <button class="btn btn-danger mx-auto px-5 mt-3" onclick="pengembalian()">Kembalikan Mobil</button>
+                        </div>
+                    <?php endif ?>
+                </div>
             </div>
-            <div class="modal-footer justify-content-between">
-                <button type="button" class="btn btn-default btn-sm" data-dismiss="modal"></button>
-                <button type="submit" class="btn btn-warning btn-sm">Ubah</button>
-            </div>
-            <?php //form_close() 
-            ?>
         </div>
     </div>
 </div>
-<?php //} 
-?>
 
 <script>
     $('#tbl_tr').DataTable({
@@ -207,6 +172,21 @@
         bFilter: false,
         bPaginate: false,
         bInfo: false
+    });
+    $('#tbl_tr3').DataTable({
+        bFilter: false,
+        bPaginate: false,
+        bInfo: false
+    });
+
+    $("#crashCar").click(function() {
+        if (document.getElementById('crashCar').checked == true) {
+            $("#denda_kerusakan").removeAttr('disabled');
+            $("#kerusakan").removeAttr('disabled');
+        } else {
+            $("#denda_kerusakan").attr('disabled', '');
+            $("#kerusakan").attr('disabled', '');
+        }
     });
 
     <?php if ($dtTransaksi['status_pinjam'] == "Dipinjam") : ?>
