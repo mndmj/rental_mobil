7864b9dd0ad00749bd454598f4241020b7148ac1
mndmj <nasrullah.03.arul@gmail.com>
2023-08-24 15:20:28 +0700

    db update

--- db/rental_mobil.sql
+++ db/rental_mobil.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 30 Jul 2023 pada 08.58
+-- Waktu pembuatan: 24 Agu 2023 pada 10.18
 -- Versi server: 10.4.22-MariaDB
--- Versi PHP: 8.1.12
+-- Versi PHP: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -44,7 +44,9 @@ CREATE TABLE `detail_user` (
 INSERT INTO `detail_user` (`id_detail_user`, `id_user`, `nik`, `nama`, `telepon`, `alamat`, `jk`) VALUES
 (1, 2, '331332870820', 'Hartono', '08968746549', 'Krpdn', 'Laki-laki'),
 (2, 3, '2524242424', 'aaa', '08979086055', 'aaa', 'Laki-laki'),
-(3, 4, '1234567890123456', 'Wahyudi', '082992826252', 'Karanganyar', 'Laki-laki');
+(3, 4, '1234567890123456', 'Wahyudi', '082992826252', 'Karanganyar', 'Laki-laki'),
+(4, 5, '331021', 'adella', '085155356023', 'Candirejo dompol', 'Perempuan'),
+(5, 6, '51384939227', 'Guruh', '08979655846', 'gedangan', 'Laki-laki');
 
 -- --------------------------------------------------------
 
@@ -73,8 +75,10 @@ CREATE TABLE `mobil` (
 
 INSERT INTO `mobil` (`id_mobil`, `nama`, `merk`, `foto_mobil`, `no_polisi`, `th_keluaran`, `bahan_bakar`, `cc_mobil`, `warna_mobil`, `harga_sewa`, `status`, `delete_at`) VALUES
 (1, 'Avanza', 'Toyota', '1690393813_592f813b6e81cbf195cf.jpg', 'AD 7657 AJ', 2010, 'Bensin', '1200', 'Hitam', 300000, 'Ada', NULL),
-(6, 'Xenia', 'Daihatsu', '', 'AD 4 SP', 2010, 'Bensin', '1000', 'Hitam', 200000, 'Ada', NULL),
-(7, 'Gg', 'honda', '1690393452_f39ede9ec7738c301b5c.jpg', 'AD 4 DI', 2010, 'Udara', '90000', 'Hitam St', 300000, 'Ada', '2023-07-27 00:51:09');
+(6, 'Xenia', 'Daihatsu', '1691943751_f78f35d9065b44b9527b.png', 'AD 4 SP', 2010, 'Bensin', '1000', 'Hitam', 200000, 'Ada', NULL),
+(7, 'Gg', 'honda', '1690393452_f39ede9ec7738c301b5c.jpg', 'AD 4 DI', 2010, 'Udara', '90000', 'Hitam St', 300000, 'Ada', '2023-07-27 00:51:09'),
+(8, 'Xenia', 'Daihatsu', '1690822152_09100ba2729c51d341e7.png', 'AD 1440 FA', 2009, 'Bensin', '1100', 'Putih', 300000, 'Ada', NULL),
+(9, 'Kijang', 'Toyota', '1690858523_6d4e783fa5908034f4e0.png', 'AD 4546 HJ', 2002, 'Solar', '1500', 'Hitam', 300000, 'Ada', NULL);
 
 -- --------------------------------------------------------
 
@@ -136,6 +140,13 @@ CREATE TABLE `transaksi_kembali` (
   `created_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data untuk tabel `transaksi_kembali`
+--
+
+INSERT INTO `transaksi_kembali` (`id_kembali`, `id_pinjam`, `tgl_kembali`, `kondisi_mobil`, `jml_denda`, `mobil_rusak`, `kerusakan`, `denda_kerusakan`, `created_at`) VALUES
+(6, 22, '2023-08-13 23:32:16', '', '0', 'Tidak', NULL, NULL, '2023-08-13 23:32:16');
+
 -- --------------------------------------------------------
 
 --
@@ -157,6 +168,16 @@ CREATE TABLE `transaksi_pinjam` (
   `id_sopir` int(11) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `transaksi_pinjam`
+--
+
+INSERT INTO `transaksi_pinjam` (`id_pinjam`, `id_user`, `id_mobil`, `nama_user`, `status_pinjam`, `tgl_pinjam`, `tgl_pesan`, `tgl_kembali`, `telepon`, `jaminan`, `sopir`, `id_sopir`, `created_at`) VALUES
+(22, NULL, 9, 'arjuna', 'Kembali', '2023-08-13 23:23:54', '2023-08-13 23:23:54', '2023-08-14 23:23:54', '08976799078', 'KTP', 'Iya', 1, '2023-08-13 23:23:54'),
+(23, NULL, 1, 'Junaidi', 'Dipinjam', '2023-08-24 14:29:37', '2023-08-24 14:29:37', '2023-08-25 14:29:37', '089898066468', 'KTP', 'Tidak', NULL, '2023-08-24 14:29:37'),
+(24, NULL, 1, 'Marni', 'Dipinjam', '2023-08-24 14:30:03', '2023-08-24 14:30:03', '2023-08-26 14:30:03', '0876697536578', 'Kendaraan Bermotor', 'Iya', NULL, '2023-08-24 14:30:03'),
+(25, NULL, 6, 'Karno', 'Booking', '2023-08-26 09:00:00', '2023-08-24 14:31:49', '2023-08-27 09:00:00', '081116565299', 'KK', 'Tidak', NULL, '2023-08-24 14:31:49');
 
 -- --------------------------------------------------------
 
@@ -181,7 +202,9 @@ INSERT INTO `user` (`id_user`, `username`, `password`, `email`, `id_role`, `dele
 (1, 'admin', '12345678', 'admin@gmail.com', 1, NULL),
 (2, 'Hartono', '12345678', 'h@g.com', 2, NULL),
 (3, 'aaa', '12345678', 'a@gmail.com', 2, NULL),
-(4, 'yudist11', '12345678', 'wahyudist123@gma', 2, NULL);
+(4, 'yudist11', '12345678', 'wahyudist123@gma', 2, NULL),
+(5, 'adell', 'adella', 'andykaoa@gmail.com', 2, NULL),
+(6, 'guruh', 'aaaaaaaa', 'g@g.com', 2, NULL);
 
 --
 -- Indexes for dumped tables
@@ -243,13 +266,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `detail_user`
 --
 ALTER TABLE `detail_user`
-  MODIFY `id_detail_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_detail_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `mobil`
 --
 ALTER TABLE `mobil`
-  MODIFY `id_mobil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_mobil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT untuk tabel `role`
@@ -267,19 +290,19 @@ ALTER TABLE `sopir`
 -- AUTO_INCREMENT untuk tabel `transaksi_kembali`
 --
 ALTER TABLE `transaksi_kembali`
-  MODIFY `id_kembali` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_kembali` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `transaksi_pinjam`
 --
 ALTER TABLE `transaksi_pinjam`
-  MODIFY `id_pinjam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_pinjam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
